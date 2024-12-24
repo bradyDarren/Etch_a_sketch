b27272cb2d6ed9b0988e026baec4915d44f7d5dc
bradyDarren <dcgbfinancial@gmail.com>
2024-12-24 13:02:03 -0600

    addition of the clockwise and counter clockwise functions

--- main.py
+++ main.py
@@ -6,11 +6,18 @@ screen = Screen()
 def move_forward():
     thomas.forward(15)
 
-def move_up():
-    thomas.setheading(90)
-    thomas.forward(15)
+def move_backward():
+    thomas.backward(15)
+
+def clockwise(): 
+    thomas.setheading(thomas.heading() + 15)
+
+def counter_clockwise():
+    thomas.setheading(thomas.heading() - 15)
 
 screen.listen()
-screen.onkey(key="space", fun=move_forward)
-screen.onkey(key="Up", fun=move_up)
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="s", fun=move_backward)
+screen.onkey(key="d", fun=clockwise)
+screen.onkey(key="a", fun=counter_clockwise)
 screen.exitonclick()
